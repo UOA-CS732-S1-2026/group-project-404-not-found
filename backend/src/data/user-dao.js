ab39c4ce83791cc40c804a91b079ccdd0628155c
Seul <sueees.now@gmail.com>
2026-04-04 16:33:31 +1300

    Commit file about user's data(hard coding), register, modify user info, login, delete

---
+++ backend/src/data/user-dao.js
@@ -0,0 +1,69 @@
+//For user and admin
+
+import bcrypt from "bcrypt";
+
+//Hard-coding
+export let users = [
+   {
+    id: 1,
+    username: "alice",
+    firstname: "Alice",
+    lastname: "Kim",
+    password: bcrypt.hashSync("alice123", 10), 
+    description: "Hello",
+    dob: "2000-01-01",
+    avatar_id: 1,
+    is_admin: 0
+  },
+  {
+    id: 2,
+    username: "bob",
+    firstname: "Bob",
+    lastname: "Lee",
+    password: bcrypt.hashSync("bob123", 10),
+    description: "Hi",
+    dob: "1999-05-05",
+    avatar_id: 2,
+    is_admin: 0
+  }
+]
+
+//Sign up
+export async function createUser(data){
+    const newUser = {
+        id: users.length +1,
+        ...data,
+        password : bcrypt.hashSync(data.password, 10),
+        is_admin: 0 
+    };
+    users.push(newUser);
+    return newUser;
+}
+
+//Login : Check Username
+export async function findUserByUsername(username){
+    return users.find(u=> u.username === username) || null;
+}
+
+//Login : Verify password
+export async function verifyUserPassword(user, password){
+    return bcrypt.compare(password, user.password);
+}
+
+//Modify user infomation
+export async function updateMyProfile(id, data){
+    const user = users.find(u=> u.id === userId);
+    if(!user) return null;
+    Object.assign(user, data);
+    return user;
+}
+
+//Delete user by themselves
+export async function deleteUserById(id){
+    const index = users.findIndex(u=>u.id === id);
+    if(index === -1) return false;
+
+    users.splice(index, 1);
+    return true;
+}
+
