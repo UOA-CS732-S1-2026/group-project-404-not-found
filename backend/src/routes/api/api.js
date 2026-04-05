66b54559f39d5eb4cb70c61f62ba66ed1002d38f
Seul <sueees.now@gmail.com>
2026-04-05 19:39:40 +1200

    Commit api.js file

--- backend/src/routes/api/api.js
+++ backend/src/routes/api/api.js
@@ -6,25 +6,27 @@ const router = express.Router();
 import authRoutes from "./api-auth.js";
 router.use("/", authRoutes);
 
-//User
-import userRoutes from "./api-admin.js";
-router.use("/users", userRoutes);
+//admin
+import adminRoutes from "./api-admin.js";
+router.use("/admin", adminRoutes);
 
 //My Profile
 import myProfileRoutes from "./api-me.js";
 router.use("/me", myProfileRoutes);
 
-// //Course
-// import courseRoutes from "./api-course.js";
-// router.use("/course", courseRoutes);
+//Marketplace
+import marketRoutes from "./api-marketplace.js";
+router.use("/marketplace", marketRoutes);
 
-// //Material
-// import materialRoutes from "./api-material.js";
-// router.use("/material", materialRoutes);
+//Material
+import materialRoutes from "./api-material.js";
+router.use("/material", materialRoutes);
 
-// //Marketplace
-// import marketRoutes from "./api-marketplace.js";
-// router.jse("/marketplace", marketRoutes);
+//Course
+import courseRoutes from "./api-course.js";
+router.use("/course", courseRoutes);
+
+
 
 export default router;
 
